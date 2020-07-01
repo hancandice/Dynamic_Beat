e84e02fd656689676f41d240d5ba81cc769e09f7
Candice Han <jeeyoung.han82@gmail.com>
2020-07-01 09:48:17 +0200

    CHANGE background image & ADD menuBar.png

--- src/dynamic_beat_4/DynamicBeat.java
+++ src/dynamic_beat_4/DynamicBeat.java
@@ -1,27 +1,36 @@
-package dynamic_beat_3;
+package dynamic_beat_4;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class DynamicBeat extends JFrame {
 
 	private Image screenImage;
 	private Graphics screenGraphic;
 
-	private Image introBackground;
-
+	private Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground(Title).jpg")).getImage();
+    private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+    		
 	public DynamicBeat() {
+		setUndecorated(true);
 		setTitle("Dynamic Beat Game");
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-
-		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground(Title).jpg")).getImage();
+		setVisible(true); 
+		setBackground(new Color(0,0,0,0));
+		setLayout(null);
+		
+		menuBar.setBounds(0,0,1280,30);
+		add(menuBar);
 
 		Music introMusic = new Music("introMusic.mp3", true);
 		introMusic.start();
